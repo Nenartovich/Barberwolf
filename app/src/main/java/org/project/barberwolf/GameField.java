cfd8e150a21415607e727d2718a5877ab846ddc2
Nenartovich <nenartovski@yandex.com>
2019-11-15 12:08:50 +0300

    add simple background

--- app/src/main/java/org/project/barberwolf/GameField.java
+++ app/src/main/java/org/project/barberwolf/GameField.java
@@ -1,6 +1,7 @@
 package org.project.barberwolf;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,6 @@ public class GameField extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_field);
     }
 }
