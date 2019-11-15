5dbd2bff091edd6d9a8323f52442d22fb2169da6
Nenartovich <nenartovski@yandex.com>
2019-11-15 10:17:50 +0300

    work with main menu buttons

--- app/src/main/java/org/project/barberwolf/MainActivity.java
+++ app/src/main/java/org/project/barberwolf/MainActivity.java
@@ -3,6 +3,8 @@ package org.project.barberwolf;
 import  androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button newGameButton = (Button) findViewById(R.id.newGameButton);
+        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to the new game screen
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to the settings screen
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+            }
+        });
     }
+
+
+
 }
