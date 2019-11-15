b624206de210120bfccf2de8cbb2f0cbf76cdf53
Nenartovich <nenartovski@yandex.com>
2019-11-15 11:30:11 +0300

    create blank second activity

---
+++ app/src/main/java/org/project/barberwolf/GameField.java
@@ -0,0 +1,13 @@
+package org.project.barberwolf;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameField extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/org/project/barberwolf/MainActivity.java
+++ app/src/main/java/org/project/barberwolf/MainActivity.java
@@ -2,9 +2,11 @@ package org.project.barberwolf;
 
 import  androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Go to the new game screen
+                Intent intent = new Intent(MainActivity.this, GameField.class);
+                startActivity(intent);
             }
         });
 
